265b8672f76f67adc019add215fff1327213c53a
Wetzel Victor <wetzel.victor@gmail.com>
2020-04-22 16:07:04 +0200

    converting methods to func

--- tmp_sympy_LDL_decomp.py
+++ tmp_sympy_LDL_decomp.py
@@ -1,14 +1,14 @@
-def _LDL_sparse(self):
+def LDL_sparse(matrix):
         """Algorithm for numeric LDL factorization, exploiting sparse structure.
         """
-        Lrowstruc = self.row_structure_symbolic_cholesky()
-        L = self.eye(self.rows)
-        D = self.zeros(self.rows, self.cols)
+        Lrowstruc = matrix.row_structure_symbolic_cholesky()
+        L = matrix.eye(matrix.rows)
+        D = matrix.zeros(matrix.rows, matrix.cols)
 
         for i in range(len(Lrowstruc)):
             for j in Lrowstruc[i]:
                 if i != j:
-                    L[i, j] = self[i, j]
+                    L[i, j] = matrix[i, j]
                     summ = 0
                     for p1 in Lrowstruc[i]:
                         if p1 < j:
@@ -35,23 +35,23 @@ def _LDL_sparse(self):
 
     return L, D
 
-def _LDL_solve(self, rhs):
+def LDL_solve(matrix, rhs):
         # for speed reasons, this is not uncommented, but if you are
         # having difficulties, try uncommenting to make sure that the
         # input matrix is symmetric
 
-        #assert self.is_symmetric()
-        L, D = self._LDL_sparse()
-        Z = L._lower_triangular_solve(rhs)
-        Y = D._diagonal_solve(Z)
-        return L.T._upper_triangular_solve(Y)
+        #assert matrix.is_symmetric()
+        L, D = LDL_sparse(matrix)
+        Z = lower_triangular_solve(L, rhs)
+        Y = diagonal_solve(D, Z)
+        return upper_triangular_solve(L, Y)).T
 
-def _lower_triangular_solve(self, rhs):
+def lower_triangular_solve(matrix, rhs):
     """Fast algorithm for solving a lower-triangular system,
     exploiting the sparsity of the given matrix.
     """
-    rows = [[] for i in range(self.rows)]
-    for i, j, v in self.row_list():
+    rows = [[] for i in range(matrix.rows)]
+    for i, j, v in matrix.row_list():
         if i > j:
             rows[i].append((j, v))
     X = rhs.as_mutable().copy()
@@ -59,14 +59,14 @@ def _lower_triangular_solve(self, rhs):
         for i in range(rhs.rows):
             for u, v in rows[i]:
                 X[i, j] -= v*X[u, j]
-            X[i, j] /= self[i, i]
-    return self._new(X)
+            X[i, j] /= matrix[i, i]
+    return matrix._new(X)
 
-def _diagonal_solve(self, rhs):
+def diagonal_solve(matrix, rhs):
     """Helper function of function diagonal_solve, without the error
     checks, to be used privately.
     """
-    return self._new(
-        rhs.rows, rhs.cols, lambda i, j: rhs[i, j] / self[i, i])
+    return matrix._new(
+        rhs.rows, rhs.cols, lambda i, j: rhs[i, j] / matrix[i, i])
 
 
